b3ee758529af0901621c94b383df6dfc8c16f593
ad3m4d <adem.awol.vm6925@gmail.com>
2025-06-18 14:53:22 +0000

    Home-view Updated

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,6 +4,7 @@ import SidebarNavigation from "./SidebarNavigation.tsx";
 import WelcomeBanner from "./WelcomeBanner.tsx";
 import PostEditor from "./post/PostEditor.tsx";
 import React from "react";
+import SidePanel from "./SidePanel.tsx";
 import ProfileCompletionBanner from "./ProfileCompletionBanner.tsx";
 function Home() {
   return (
@@ -15,9 +16,12 @@ function Home() {
         </HeaderNavigation>
         <div className="flex">
           <SidebarNavigation className="items-start w-max justify-start flex sticky h-screen absolute static" />
-          <WelcomeBanner className=" h-screen w-screen justify-center items-start static">
+          <WelcomeBanner className=" h-screen justify-center items-start static w-full">
             <PostEditor />
           </WelcomeBanner>
+          <SidePanel className="w-[680px] h-screen">
+            SidePanel content
+          </SidePanel>
         </div>
       </div>
     </div>
